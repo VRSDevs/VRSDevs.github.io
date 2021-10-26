993db4bde2f3c987e0902b85329626190f424fc0
rox06io <rox06io.games@gmail.com>
2021-10-26 11:45:26 +0200

    porfolio v2

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -52,3 +52,58 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+// cambiar idioma
+let langs = ['sp', 'eng'];
+let lang = 'sp';
+setLangStyles(lang);
+
+function setStyles(styles) {
+  var elementId = '__lang_styles';
+  var element = document.getElementById(elementId);
+  if (element) {
+    element.remove();
+  }
+
+  let style = document.createElement('style');
+  style.id = elementId;
+  style.type = 'text/css';
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = styles;
+  } else {
+    style.appendChild(document.createTextNode(styles));
+  }
+  document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+function setLang(lang) {
+  setLangStyles(lang);
+  ChangeColor(lang);
+  console.log("Done");
+}
+function ChangeColor(lang){
+    if(lang === 'sp'){
+        document.getElementById('sp').style.color = "#1a2a3a";
+        document.getElementById('eng').style.color = "#fff";
+    } else{
+        document.getElementById('sp').style.color = "#fff";
+        document.getElementById('eng').style.color = "#1a2a3a";
+    }
+}
+
+function setLangStyles(lang) {
+  let styles = langs
+    .filter(function (l) {
+      return l != lang;
+    })
+    .map(function (l) {
+      return ':lang('+ l +') { display: none; }';
+    })
+    .join(' ');
+
+  setStyles(styles);
+}
+
+window.onload = function(){
+    ChangeColor("sp");
+}
